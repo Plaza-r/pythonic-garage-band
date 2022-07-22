dbd56d159b170df038ebc81a2f6a6ce1bb1a241c
RIKI PLAZA <rikiplaza1@gmail.com>
2022-07-22 00:21:34 -0400

    will continue tomorrow

---
+++ pythonic_garage_band/band.py
@@ -0,0 +1,30 @@
+class Band():
+    instances = []
+
+    def __init__(self, name, arr):
+        self.name = name
+        self.members = arr
+        Band.instances.append(self)
+
+def play_solos(self):
+    return ["face melting guitar solo", "bom bom buh bom", "rattle boom crash"]
+
+    @classmethod
+    def to_(cls):
+        return cls.instances
+
+
+class Musician:
+    pass
+
+
+class Guitarist:
+    pass
+
+
+class Bassist:
+    pass
+
+
+class Drummer:
+    pass
